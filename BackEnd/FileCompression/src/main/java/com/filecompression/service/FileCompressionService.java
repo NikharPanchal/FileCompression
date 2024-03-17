555cbba8a99a86d474a473a7070ca3d14780171a
NikharPanchal <Panchalnikhar4920@gmail.com>
2024-03-17 13:31:59 +0530

    update api

--- BackEnd/FileCompression/src/main/java/com/filecompression/service/FileCompressionService.java
+++ BackEnd/FileCompression/src/main/java/com/filecompression/service/FileCompressionService.java
@@ -2,6 +2,9 @@ package com.filecompression.service;
 
 import java.io.InputStream;
 
+import com.filecompression.entity.FileCompression;
+import com.filecompression.entity.FileVo;
+
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface FileCompressionService {
@@ -9,4 +12,8 @@ public interface FileCompressionService {
 	public byte[] addContentToZipFile(InputStream inputStream, String fileName);
 	
 	public void saveZipFile(byte[] zipContent, String filename, HttpServletResponse response);
+	
+	public FileVo saveZipContentToDatabase(byte[] zipContent, String filename, String Path);
+
+	public FileCompression getFilebyFileId(String fileId);
 }
